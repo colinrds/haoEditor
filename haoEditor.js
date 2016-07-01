c59127581f27dadc93453444e49a393590ab39a1
童俊豪 <hao@bluereader.cn>
2016-07-01 18:37:41 +0800

    add

--- haoEditor.js
+++ haoEditor.js
@@ -19,13 +19,13 @@
 			text_left:true,
 			text_right:true,
 			text_center:true,
-			font_size:true,
-			font_color:true,
+			font_size:false,
+			font_color:false,
 			add_link:true,
 			undo:true
 		},options);
 
-		var fontSize = [["Header1","1"],["Header2","2"],["Header3","3"],["Header4","4"],["Header4","4"],["Header5","5"],["Header6","6"],["Header7","7"]];
+		var fontSize = [["Size1","1"],["Size2","2"],["Size3","3"],["Size4","4"],["Size4","4"],["Size5","5"],["Size6","6"],["Size7","7"]];
 		this.wrap('<div class="haoEditor"></div>');
 		var haoEditor = this.parents(".haoEditor");
 		init();
@@ -44,11 +44,17 @@
 					var tool_div = $("<div><a href='javascript:void(0);' class="+ key +">"+ vars[key][1] +"</a></div>")
 					switch (key) {
 						case "font_size":
-							var font_div = $("<div class='hao_formats'></div>");
+							var font_div = $("<div class='toolBox'></div>");
 								$.each(fontSize,function(index, font) {
-									font_div.append("<a hao-formatsval="+font[1]+">"+ font[0] +"</a>")
+									font_div.append("<a hao-param="+font[1]+"><font size="+font[1]+">"+ font[0] +"</font></a>")
 								});
 								tool_div.append(font_div);
+							break;
+						case "add_link":
+							var link_div = $("<div class='toolBox'></div>");
+								link_div.append("<input type='text' class='data' placeholder='请输入链接地址' value='http://'>");
+								link_div.append("<div class='toolbuttons'><input class='confirm' type='button' value='确定'><input class='cancel' type='button' value='取消'></div>");
+								tool_div.append(link_div);
 							break;
 					}
 					toolbar.append(tool_div);
@@ -62,50 +68,64 @@
 
 		
 		function bindEvent(){
-			$(".haoEditor .toolbar a").click(function(){
-				cammandOne($(this));
-			})
+			$(".haoEditor .toolbar>div>a").click(function(){
+				var command = $(this).attr("class");
+				switch (command) {
+					case "font_size":
+						$(this).next("div").slideToggle("fast");
+						break;
+					case "add_link":
+						$(this).next("div").slideToggle("fast");
+						break;
+					default: 
+						cammandOne($(this),command);
+						break;
+				}
+			});
+
+			$(".haoEditor .toolbar>div .confirm").click(function(){
+				var command = $(this).parents(".toolBox").prev("a").attr("class");
+				// cammandTwo($(this),command);
+				setLink();
+			});
+
+			$(".haoEditor .toolbar .toolbuttons input").click(function() {
+				$(this).parents(".toolBox").slideUp("fast");
+			});
 		}
 
-		// 无下拉框的指令
-		function cammandOne(obj){
-				var command = obj.attr("class");
-				command = vars[command][0];
-				operation(command);
+		// 无需第二参数的指令
+		function cammandOne(obj,command){
+			command = vars[command][0];
+			operation(command);
+		}
+
+		// 有二参数的指令
+		function cammandTwo(obj,command){
+			command = vars[command][0];
+			var secondParam = obj.parent(".toolbuttons").prev("input").val();
+			operation(command,secondParam);
 		}
 		function operation(command,secondParam){
 			document.execCommand(command, false, secondParam);
 		}
+
+		// 获得选中对象
+		function getSelection(){
+			var selection = "";
+			// Chrome
+			if(document.getSelection){
+				selection = document.getSelection();
+			}
+			//IE
+			else{
+				selection = document.selection;
+			}
+			
+			return selection;
+		}
+		function setLink(){
+
+		}
 	}
 })(jQuery);
-
-// $(function(){
-// 	var vars = {
-// 		bold:"Bold",
-// 		italic:"Italic",
-// 		text_left:"JustifyLeft",
-// 		text_right:"JustifyRight",
-// 		text_center:"JustifyCenter",
-// 		font_size:"FontSize",
-// 		font_color:"ForeColor",
-// 		add_link:"CreateLink",
-// 		undo:"Undo"
-// 	}
-// 	var support = {
-// 		bold:true,
-// 		bold:true,
-// 		italic:true,
-// 		text_left:true,
-// 		text_right:true,
-// 		text_center:true,
-// 		font_size:true,
-// 		font_color:true,
-// 		add_link:true,
-// 		undo:true
-// 	}
-// 	$(".toolbar a").click(function(){
-// 		var cammand = $(this)[0].className;
-// 		cammand = vars[cammand];
-// 		document.execCommand(cammand, false, false);
-// 	});
-// });
